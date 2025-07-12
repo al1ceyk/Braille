ebbef81c462c2ac10de97ca0f10ce26f7b247b73
김예원 <alice@gim-yewon-ui-MacBookAir.local>
2025-07-12 12:07:18 +0900

    complete data.py.

--- data.py
+++ data.py
@@ -1,30 +1,30 @@
 # English Braille
-english_alpha_1 = {'english' : 'a', 'braille' : [1, 0, 0, 0, 0, 0]}
-english_alpha_2 = {'english' : 'b', 'braille' : [1, 1, 0, 0, 0, 0]}
-english_alpha_3 = {'english' : 'c', 'braille' : [1, 0, 0, 1, 0, 0]}
-english_alpha_4 = {'english' : 'd', 'braille' : [1, 0, 0, 1, 1, 0]}
-english_alpha_5 = {'english' : 'e', 'braille' : [1, 0, 0, 0, 1, 0]}
-english_alpha_6 = {'english' : 'f', 'braille' : [1, 1, 0, 1, 0, 0]}
-english_alpha_7 = {'english' : 'g', 'braille' : [1, 1, 0, 1, 1, 0]}
-english_alpha_8 = {'english' : 'h', 'braille' : [1, 1, 0, 0, 1, 0]}
-english_alpha_9 = {'english' : 'i', 'braille' : [0, 1, 0, 4, 0, 0]}
-english_alpha_10 = {'english' : 'j', 'braille' : [0, 1, 0, 1, 1, 0]}
-english_alpha_11 = {'english' : 'k', 'braille' : [1, 0, 1, 0, 0, 0]}
-english_alpha_12 = {'english' : 'l', 'braille' : [1, 1, 1, 0, 0, 0]}
-english_alpha_13 = {'english' : 'm', 'braille' : [1, 0, 1, 1, 0, 0]}
-english_alpha_14 = {'english' : 'n', 'braille' : [1, 0, 1, 1, 1, 0]}
-english_alpha_15 = {'english' : 'o', 'braille' : [1, 0, 1, 0, 1, 0]}
-english_alpha_16 = {'english' : 'p', 'braille' : [1, 1, 1, 1, 0, 0]}
-english_alpha_17 = {'english' : 'q', 'braille' : [1, 1, 1, 1, 1, 0]}
-english_alpha_18 = {'english' : 'r', 'braille' : [1, 1, 1, 0, 1, 0]}
-english_alpha_19 = {'english' : 's', 'braille' : [0, 1, 1, 1, 0, 0]}
-english_alpha_20 = {'english' : 't', 'braille' : [0, 1, 1, 1, 1, 0]}
-english_alpha_21 = {'english' : 'u', 'braille' : [1, 0, 1, 0, 0, 1]}
-english_alpha_22 = {'english' : 'v', 'braille' : [1, 1, 1, 0, 0, 1]}
-english_alpha_23 = {'english' : 'w', 'braille' : [0, 1, 0, 1, 1, 1]}
-english_alpha_24 = {'english' : 'x', 'braille' : [1, 0, 1, 1, 0, 1]}
-english_alpha_25 = {'english' : 'y', 'braille' : [1, 0, 1, 1, 1, 1]}
-english_alpha_26 = {'english' : 'z', 'braille' : [1, 0, 1, 0, 1, 1]}
+english_alpha_1 = {'english' : 'a', 'number' : 1, 'braille' : [[1, 0, 0, 0, 0, 0]]}
+english_alpha_2 = {'english' : 'b', 'number' : 1, 'braille' : [[1, 1, 0, 0, 0, 0]]}
+english_alpha_3 = {'english' : 'c', 'number' : 1, 'braille' : [[1, 0, 0, 1, 0, 0]]}
+english_alpha_4 = {'english' : 'd', 'number' : 1, 'braille' : [[1, 0, 0, 1, 1, 0]]}
+english_alpha_5 = {'english' : 'e', 'number' : 1, 'braille' : [[1, 0, 0, 0, 1, 0]]}
+english_alpha_6 = {'english' : 'f', 'number' : 1, 'braille' : [[1, 1, 0, 1, 0, 0]]}
+english_alpha_7 = {'english' : 'g', 'number' : 1, 'braille' : [[1, 1, 0, 1, 1, 0]]}
+english_alpha_8 = {'english' : 'h', 'number' : 1, 'braille' : [[1, 1, 0, 0, 1, 0]]}
+english_alpha_9 = {'english' : 'i', 'number' : 1, 'braille' : [[0, 1, 0, 1, 0, 0]]}
+english_alpha_10 = {'english' : 'j', 'number' : 1, 'braille' : [[0, 1, 0, 1, 1, 0]]}
+english_alpha_11 = {'english' : 'k', 'number' : 1, 'braille' : [[1, 0, 1, 0, 0, 0]]}
+english_alpha_12 = {'english' : 'l', 'number' : 1, 'braille' : [[1, 1, 1, 0, 0, 0]]}
+english_alpha_13 = {'english' : 'm', 'number' : 1, 'braille' : [[1, 0, 1, 1, 0, 0]]}
+english_alpha_14 = {'english' : 'n', 'number' : 1, 'braille' : [[1, 0, 1, 1, 1, 0]]}
+english_alpha_15 = {'english' : 'o', 'number' : 1, 'braille' : [[1, 0, 1, 0, 1, 0]]}
+english_alpha_16 = {'english' : 'p', 'number' : 1, 'braille' : [[1, 1, 1, 1, 0, 0]]}
+english_alpha_17 = {'english' : 'q', 'number' : 1, 'braille' : [[1, 1, 1, 1, 1, 0]]}
+english_alpha_18 = {'english' : 'r', 'number' : 1, 'braille' : [[1, 1, 1, 0, 1, 0]]}
+english_alpha_19 = {'english' : 's', 'number' : 1, 'braille' : [[0, 1, 1, 1, 0, 0]]}
+english_alpha_20 = {'english' : 't', 'number' : 1, 'braille' : [[0, 1, 1, 1, 1, 0]]}
+english_alpha_21 = {'english' : 'u', 'number' : 1, 'braille' : [[1, 0, 1, 0, 0, 1]]}
+english_alpha_22 = {'english' : 'v', 'number' : 1, 'braille' : [[1, 1, 1, 0, 0, 1]]}
+english_alpha_23 = {'english' : 'w', 'number' : 1, 'braille' : [[0, 1, 0, 1, 1, 1]]}
+english_alpha_24 = {'english' : 'x', 'number' : 1, 'braille' : [[1, 0, 1, 1, 0, 1]]}
+english_alpha_25 = {'english' : 'y', 'number' : 1, 'braille' : [[1, 0, 1, 1, 1, 1]]}
+english_alpha_26 = {'english' : 'z', 'number' : 1, 'braille' : [[1, 0, 1, 0, 1, 1]]}
 english_alpha = [
     english_alpha_1, 
     english_alpha_2, 
@@ -53,8 +53,278 @@ english_alpha = [
     english_alpha_25, 
     english_alpha_26]
 
-# Abbreviated
+# Acronym/Abbreviated
+english_acr_1 = {'english' : 'but', 'number' : 1, 'braille' : [[1, 1, 0, 0, 0, 0]]}
+english_acr_2 = {'english' : 'can', 'number' : 1, 'braille' : [[1, 0, 0, 1, 0, 0]]}
+english_acr_3 = {'english' : 'do', 'number' : 1, 'braille' : [[1, 0, 0, 1, 1, 0]]}
+english_acr_4 = {'english' : 'every', 'number' : 1, 'braille' : [[1, 0, 0, 0, 1, 0]]}
+english_acr_5 = {'english' : 'from', 'number' : 1, 'braille' : [[1, 1, 0, 1, 0, 0]]}
+english_acr_6 = {'english' : 'self', 'number' : 1, 'braille' : [[1, 1, 0, 1, 0, 0]]}
+english_acr_7 = {'english' : 'go', 'number' : 1, 'braille' : [[1, 1, 0, 1, 1, 0]]}
+english_acr_8 = {'english' : 'have', 'number' : 1, 'braille' : [[1, 1, 0, 0, 1, 0]]}
+english_acr_9 = {'english' : 'just', 'number' : 1, 'braille' : [[0, 1, 0, 1, 1, 0]]}
+english_acr_10 = {'english' : 'knowledge', 'number' : 1, 'braille' : [[1, 0, 1, 0, 0, 0]]}
+english_acr_11 = {'english' : 'like', 'number' : 1, 'braille' : [[1, 1, 1, 0, 0, 0]]}
+english_acr_12 = {'english' : 'more', 'number' : 1, 'braille' : [[1, 0, 1, 1, 0, 0]]}
+english_acr_13 = {'english' : 'not', 'number' : 1, 'braille' : [[1, 0, 1, 1, 1, 0]]}
+english_acr_14 = {'english' : 'people', 'number' : 1, 'braille' : [[1, 1, 1, 1, 0, 0]]}
+english_acr_15 = {'english' : 'quite', 'number' : 1, 'braille' : [[1, 1, 1, 1, 1, 0]]}
+english_acr_16 = {'english' : 'rather', 'number' : 1, 'braille' : [[1, 1, 1, 0, 1, 0]]}
+english_acr_17 = {'english' : 'so', 'number' : 1, 'braille' : [[0, 1, 1, 1, 0, 0]]}
+english_acr_18 = {'english' : 'that', 'number' : 1, 'braille' : [[0, 1, 1, 1, 1, 0]]}
+english_acr_19 = {'english' : 'still', 'number' : 1, 'braille' : [[0, 0, 1, 1, 0, 0]]}
+english_acr_20 = {'english' : 'us', 'number' : 1, 'braille' : [[1, 0, 1, 0, 0, 1]]}
+english_acr_21 = {'english' : 'very', 'number' : 1, 'braille' : [[1, 1, 1, 0, 0, 1]]}
+english_acr_22 = {'english' : 'it', 'number' : 1, 'braille' : [[1, 0, 1, 1, 0, 1]]}
+english_acr_23 = {'english' : 'you', 'number' : 1, 'braille' : [[1, 0, 1, 1, 1, 1]]}
+english_acr_24 = {'english' : 'as', 'number' : 1, 'braille' : [[1, 0, 1, 0, 1, 1]]}
+english_acr_25 = {'english' : 'child', 'number' : 1, 'braille' : [[1, 0, 0, 0, 0, 1]]}
+english_acr_26 = {'english' : 'shall', 'number' : 1, 'braille' : [[1, 0, 0, 1, 0, 1]]}
+english_acr_27 = {'english' : 'this', 'number' : 1, 'braille' : [[1, 0, 0, 1, 1, 1]]}
+english_acr_28 = {'english' : 'which', 'number' : 1, 'braille' : [[1, 0, 0, 0, 1, 1]]}
+english_acr_29 = {'english' : 'out', 'number' : 1, 'braille' : [[1, 1, 0, 0, 1, 1]]}
+english_acr_30 = {'english' : 'will', 'number' : 1, 'braille' : [[0, 1, 0, 1, 1, 1]]}
+# be/be- ?
+english_acr_31 = {'english' : 'be', 'number' : 1, 'braille' : [[0, 1, 1, 0, 0, 0]]}
+english_acr_32 = {'english' : 'con', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 0]]}
+english_acr_33 = {'english' : 'dis', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 1]]}
+english_acr_34 = {'english' : 'enough', 'number' : 1, 'braille' : [[0, 1, 0, 0, 0, 1]]}
+english_acr_35 = {'english' : 'to', 'number' : 1, 'braille' : [[0, 1, 1, 0, 1, 0]]}
+english_acr_36 = {'english' : 'were', 'number' : 1, 'braille' : [[0, 1, 1, 0, 1, 1]]}
+english_acr_37 = {'english' : 'his', 'number' : 1, 'braille' : [[0, 1, 1, 0, 0, 1]]}
+english_acr_38 = {'english' : 'by', 'number' : 1, 'braille' : [[0, 0, 1, 0, 1, 1]]}
+english_acr_39 = {'english' : 'was', 'number' : 1, 'braille' : [[0, 0, 1, 0, 1, 1]]}
+english_acr_40 = {'english' : 'com', 'number' : 1, 'braille' : [[0, 0, 1, 0, 0, 1]]}
+english_acr_41 = {'english' : 'into', 'number' : 2, 'braille' : [[0, 0, 1, 0, 1, 0], [0, 1, 1, 0, 1, 0]]}
+english_acr = [
+    english_acr_1, 
+    english_acr_2,
+    english_acr_3,
+    english_acr_4,
+    english_acr_5,
+    english_acr_6,
+    english_acr_7,
+    english_acr_8,
+    english_acr_9,
+    english_acr_10,
+    english_acr_11,
+    english_acr_12,
+    english_acr_13,
+    english_acr_14,
+    english_acr_15,
+    english_acr_16,
+    english_acr_17,
+    english_acr_18,
+    english_acr_19,
+    english_acr_20,
+    english_acr_21,
+    english_acr_22,
+    english_acr_23,
+    english_acr_24,
+    english_acr_25,
+    english_acr_26,
+    english_acr_27,
+    english_acr_28,
+    english_acr_29,
+    english_acr_30,
+    english_acr_31,
+    english_acr_32,
+    english_acr_33,
+    english_acr_34,
+    english_acr_35,
+    english_acr_36,
+    english_acr_37,
+    english_acr_38,
+    english_acr_39,
+    english_acr_40,
+    english_acr_41]
+
+english_abb_1 = {'english' : 'and', 'number' : 1, 'braille' : [[1, 1, 1, 1, 0, 1]]}
+english_abb_2 = {'english' : 'for', 'number' : 1, 'braille' : [[1, 1, 1, 1, 1, 1]]}
+english_abb_3 = {'english' : 'of', 'number' : 1, 'braille' : [[1, 1, 1, 0, 1, 1]]}
+english_abb_4 = {'english' : 'the', 'number' : 1, 'braille' : [[0, 1, 1, 1, 0, 1]]}
+english_abb_5 = {'english' : 'with', 'number' : 1, 'braille' : [[0, 1, 1, 1, 1, 1]]}
+english_abb_6 = {'english' : 'ch', 'number' : 1, 'braille' : [[1, 0, 0, 0, 0, 1]]}
+english_abb_7 = {'english' : 'gh', 'number' : 1, 'braille' : [[1, 1, 0, 0, 0, 1]]}
+english_abb_8 = {'english' : 'sh', 'number' : 1, 'braille' : [[1, 0, 0, 1, 0, 1]]}
+english_abb_9 = {'english' : 'th', 'number' : 1, 'braille' : [[1, 0, 0, 1, 1, 1]]}
+english_abb_10 = {'english' : 'wh', 'number' : 1, 'braille' : [[1, 0, 0, 0, 1, 1]]}
+english_abb_11 = {'english' : 'ed', 'number' : 1, 'braille' : [[1, 1, 0, 1, 0, 1]]}
+english_abb_12 = {'english' : 'er', 'number' : 1, 'braille' : [[1, 1, 0, 1, 1, 1]]}
+english_abb_13 = {'english' : 'ou', 'number' : 1, 'braille' : [[1, 1, 0, 0, 1, 1]]}
+english_abb_14 = {'english' : 'ow', 'number' : 1, 'braille' : [[0, 1, 0, 1, 0, 1]]}
+english_abb_15 = {'english' : 'en', 'number' : 1, 'braille' : [[0, 1, 0, 0, 0, 1]]}
+english_abb_16 = {'english' : 'in', 'number' : 1, 'braille' : [[0, 0, 1, 0, 1, 0]]}
+english_abb_17 = {'english' : 'st', 'number' : 1, 'braille' : [[0, 0, 1, 1, 0, 0]]}
+english_abb_18 = {'english' : 'ar', 'number' : 1, 'braille' : [[0, 0, 1, 1, 1, 0]]}
+english_abb_19 = {'english' : 'ing', 'number' : 1, 'braille' : [[0, 0, 1, 1, 0, 1]]}
+english_abb_20 = {'english' : 'ble', 'number' : 1, 'braille' : [[0, 0, 1, 1, 1, 1]]}
+english_abb_21 = {'english' : 'ea', 'number' : 1, 'braille' : [[0, 1, 0, 0, 0, 0]]}
+english_abb_22 = {'english' : 'bb', 'number' : 1, 'braille' : [[0, 1, 1, 0, 0, 0]]}
+english_abb_23 = {'english' : 'cc', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 0]]}
+english_abb_24 = {'english' : 'dd', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 1]]}
+english_abb_25 = {'english' : 'ff', 'number' : 1, 'braille' : [[0, 1, 1, 0, 1, 0]]}
+english_abb_26 = {'english' : 'gg', 'number' : 1, 'braille' : [[0, 1, 1, 0, 1, 1]]}
+english_abb = [
+    english_abb_1, 
+    english_abb_2,
+    english_abb_3,
+    english_abb_4,
+    english_abb_5,
+    english_abb_6,
+    english_abb_7,
+    english_abb_8,
+    english_abb_9,
+    english_abb_10,
+    english_abb_11,
+    english_abb_12,
+    english_abb_13,
+    english_abb_14,
+    english_abb_15,
+    english_abb_16,
+    english_abb_17,
+    english_abb_18,
+    english_abb_19,
+    english_abb_20,
+    english_abb_21,
+    english_abb_22,
+    english_abb_23,
+    english_abb_24,
+    english_abb_25,
+    english_abb_26]
+
 # Number/Math
-english_num_1 = {'english' : '1', 'braille' : [1, 0, 0, 0, 0, 0]}
+english_num_1 = {'english' : '1', 'number' : 1, 'braille' : [[1, 0, 0, 0, 0, 0]]}
+english_num_2 = {'english' : '2', 'number' : 1, 'braille' : [[1, 1, 0, 0, 0, 0]]}
+english_num_3 = {'english' : '3', 'number' : 1, 'braille' : [[1, 0, 0, 1, 0, 0]]}
+english_num_4 = {'english' : '4', 'number' : 1, 'braille' : [[1, 0, 0, 1, 1, 0]]}
+english_num_5 = {'english' : '5', 'number' : 1, 'braille' : [[1, 0, 0, 0, 1, 0]]}
+english_num_6 = {'english' : '6', 'number' : 1, 'braille' : [[1, 1, 0, 1, 0, 0]]}
+english_num_7 = {'english' : '7', 'number' : 1, 'braille' : [[1, 1, 0, 1, 1, 0]]}
+english_num_8 = {'english' : '8', 'number' : 1, 'braille' : [[1, 1, 0, 0, 1, 0]]}
+english_num_9 = {'english' : '9', 'number' : 1, 'braille' : [[0, 1, 0, 1, 0, 0]]}
+english_num_10 = {'english' : '0', 'number' : 1, 'braille' : [[0, 1, 0, 1, 1, 0]]}
+english_num = [
+    english_num_1, 
+    english_num_2,
+    english_num_3,
+    english_num_4,
+    english_num_5,
+    english_num_6,
+    english_num_7,
+    english_num_8,
+    english_num_9,
+    english_num_10]
+
+# 특수문자 변 필요.
+english_math_1 = {'english' : '+', 'number' : 1, 'braille' : [[0, 0, 0, 0, 1, 0]]}
+english_math_2 = {'english' : '-', 'number' : 1, 'braille' : [[0, 0, 1, 0, 0, 1]]}
+english_math_3 = {'english' : '*', 'number' : 1, 'braille' : [[0, 1, 1, 0, 0, 1]]}
+english_math_4 = {'english' : '.', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 1]]}
+english_math_5 = {'english' : '/', 'number' : 1, 'braille' : [[0, 0, 1, 1, 0, 0]]}
+english_math_6 = {'english' : '=', 'number' : 1, 'braille' : [[0, 1, 1, 0, 1, 1]]}
+english_math_7 = {'english' : '<', 'number' : 1, 'braille' : [[1, 1, 0, 0, 0, 1]]}
+english_math_8 = {'english' : '>', 'number' : 1, 'braille' : [[0, 0, 1, 1, 1, 0]]}
+english_math_9 = {'english' : '.', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 1]]}
+english_math_10 = {'english' : ',', 'number' : 1, 'braille' : [[0, 1, 0, 0, 0, 0]]}
+english_math = [
+    english_math_1, 
+    english_math_2,
+    english_math_3,
+    english_math_4,
+    english_math_5,
+    english_math_6,
+    english_math_7,
+    english_math_8,
+    english_math_9,
+    english_math_10]
+
 # Punctuation marks
+english_punc_1 = {'english' : '.', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 1]]}
+english_punc_2 = {'english' : ',', 'number' : 1, 'braille' : [[0, 1, 0, 0, 0, 0]]}
+english_punc_3 = {'english' : ';', 'number' : 1, 'braille' : [[0, 1, 1, 0, 0, 0]]}
+english_punc_4 = {'english' : ':', 'number' : 1, 'braille' : [[0, 1, 0, 0, 1, 0]]}
+english_punc_5 = {'english' : '\'', 'number' : 1, 'braille' : [[0, 0, 1, 0, 0, 0]]}
+english_punc_6 = {'english' : '-', 'number' : 1, 'braille' : [[0, 0, 1, 0, 0, 1]]}
+english_punc_7 = {'english' : '-', 'number' : 2, 'braille' : [[0, 0, 0, 0, 0, 1], [0, 0, 1, 0, 0, 1]]}
+english_punc_8 = {'english' : '_', 'number' : 2, 'braille' : [[0, 0, 0, 1, 0, 1], [0, 0, 1, 0, 0, 1]]}
+english_punc_9 = {'english' : '-', 'number' : 3, 'braille' : [[0, 0, 0, 0, 1, 0], [0, 0, 0, 0, 0, 1], [0, 0, 1, 0, 0, 1]]}
+english_punc_10 = {'english' : '...', 'number' : 3, 'braille' : [[0, 1, 0, 0, 1, 1], [0, 1, 0, 0, 1, 1], [0, 1, 0, 0, 1, 1]]}
+english_punc_11 = {'english' : '?', 'number' : 1, 'braille' : [[0, 1, 1, 0, 0, 1]]}
+english_punc_12 = {'english' : '!', 'number' : 1, 'braille' : [[0, 1, 1, 0, 1, 0]]}
+# 열고 닫는거 구분
+english_punc_13 = {'english' : '\"', 'number' : 1, 'braille' : [[0, 1, 1, 0, 0, 1]]}
+english_punc_14 = {'english' : '\"', 'number' : 1, 'braille' : [[0, 0, 1, 0, 1, 1]]}
+english_punc_15 = {'english' : '\'', 'number' : 2, 'braille' : [[0, 0, 0, 0, 0, 1], [0, 1, 1, 0, 0, 1]]}
+english_punc_16 = {'english' : '\'', 'number' : 2, 'braille' : [[0, 0, 0, 0, 0, 1], [0, 0, 1, 0, 1, 1]]}
+english_punc_17 = {'english' : '(', 'number' : 2, 'braille' : [[0, 0, 0, 0, 1, 0], [1, 1, 0, 0, 0, 1]]}
+english_punc_18 = {'english' : ')', 'number' : 2, 'braille' : [[0, 0, 0, 0, 1, 0], [0, 0, 1, 1, 1, 0]]}
+english_punc_19 = {'english' : '/', 'number' : 2, 'braille' : [[0, 0, 0, 1, 1, 1], [0, 0, 1, 1, 0, 0]]}
+english_punc_20 = {'english' : '\\', 'number' : 2, 'braille' : [[0, 0, 0, 1, 1, 1], [1, 0, 0, 0, 0, 1]]}
+english_punc = [
+    english_punc_1, 
+    english_punc_2,
+    english_punc_3,
+    english_punc_4,
+    english_punc_5,
+    english_punc_6,
+    english_punc_7,
+    english_punc_8,
+    english_punc_9,
+    english_punc_10,
+    english_punc_11,
+    english_punc_12,
+    english_punc_13,
+    english_punc_14,
+    english_punc_15,
+    english_punc_16,
+    english_punc_17,
+    english_punc_18,
+    english_punc_19,
+    english_punc_20]
+
 # Others
+# Symbols
+english_symb_1 = {'english' : '&', 'number' : 2, 'braille' : [[0, 0, 0, 1, 0, 0], [1, 1, 1, 1, 0, 1]]}
+english_symb_2 = {'english' : '#', 'number' : 2, 'braille' : [[0, 0, 0, 1, 1, 1], [1, 0, 0, 1, 1, 1]]}
+english_symb_3 = {'english' : '@', 'number' : 2, 'braille' : [[0, 0, 0, 1, 0, 0], [1, 0, 0, 0, 0, 0]]}
+english_symb_4 = {'english' : '%', 'number' : 2, 'braille' : [[0, 0, 0, 1, 0, 1], [0, 0, 1, 0, 1, 1]]}
+english_symb_5 = {'english' : '*', 'number' : 2, 'braille' : [[0, 0, 0, 0, 1, 0], [0, 0, 1, 0, 1, 0]]}
+english_symb = [
+    english_symb_1, 
+    english_symb_2,
+    english_symb_3,
+    english_symb_4,
+    english_symb_5]
+
+# Indicators
+english_indic_1 = {'english' : 'Numeric indicator', 'number' : 1, 'braille' : [[0, 0, 1, 1, 1, 1]]}
+english_indic_2 = {'english' : 'Capital letter', 'number' : 1, 'braille' : [[0, 0, 0, 0, 0, 1]]}
+english_indic_3 = {'english' : 'Capital Word', 'number' : 2, 'braille' : [[0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 1]]}
+english_indic_4 = {'english' : 'Capital passage', 'number' : 3, 'braille' : [[0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 1], [0, 0, 0, 0, 0, 1]]}
+english_indic_5 = {'english' : 'Capital terminator', 'number' : 2, 'braille' : [[0, 0, 0, 0, 0, 1], [0, 0, 1, 0, 0, 0]]}
+english_indic_6 = {'english' : 'Grade 1 Symbol', 'number' : 1, 'braille' : [[0, 0, 0, 0, 1, 1]]}
+english_indic_7 = {'english' : 'Grade 1 word', 'number' : 2, 'braille' : [[0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 1, 1]]}
+english_indic_8 = {'english' : 'Grade 1 passage', 'number' : 3, 'braille' : [[0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 1, 1]]}
+english_indic_9 = {'english' : 'Grade 1 terminator', 'number' : 2, 'braille' : [[0, 0, 0, 0, 1, 1], [0, 0, 1, 0, 0, 0]]}
+english_indic = [
+    english_indic_1, 
+    english_indic_2,
+    english_indic_3,
+    english_indic_4,
+    english_indic_5,
+    english_indic_6,
+    english_indic_7,
+    english_indic_8,
+    english_indic_9]
+
+# Currency
+english_curr_1 = {'english' : '??', 'number' : 1, 'braille' : [[1, 1, 1, 0, 0, 0]]}
+english_curr_2 = {'english' : '??', 'number' : 1, 'braille' : [[1, 0, 0, 0, 1, 0]]}
+english_curr_3 = {'english' : '??', 'number' : 1, 'braille' : [[0, 1, 1, 1, 0, 0]]}
+english_curr_4 = {'english' : '??', 'number' : 1, 'braille' : [[1, 0, 0, 1, 0, 0]]}
+english_curr = [
+    english_curr_1, 
+    english_curr_2, 
+    english_curr_3, 
+    english_curr_4]
--- test.py
+++ test.py
@@ -1,36 +1,40 @@
-from data import *
+from data import english_alpha, english_acr, english_abb
 
-def test_braille():
-    for english in english_alpha:
-        print(english['english'])
-        braille = english['braille']
-        if braille[0] == 1:
-            print('O', end=' ')
-        else:
-            print('X', end= ' ')
-        
-        if braille[3] == 1:
-            print('O')
-        else:
-            print('X')
-        
-        if braille[1] == 1:
-            print('O', end=' ')
-        else:
-            print('X', end= ' ')
-        
-        if braille[4] == 1:
-            print('O')
-        else:
-            print('X')
-        
-        if braille[2] == 1:
-            print('O', end=' ')
-        else:
-            print('X', end= ' ')
-        
-        if braille[5] == 1:
-            print('O')
-        else:
-            print('X')
+def test_braille(brailles_list):
+    for braille_dict in brailles_list:
+        print(braille_dict['english'])
+        braille = braille_dict['braille']
+        for i in range(braille_dict['number']):
+            if braille[i][0] == 1:
+                print('O', end=' ')
+            else:
+                print('X', end=' ')
+            
+            if braille[i][3] == 1:
+                print('O', end=' ')
+            else:
+                print('X', end=' ')
+        print()
+        for i in range(braille_dict['number']):
+            if braille[i][1] == 1:
+                print('O', end=' ')
+            else:
+                print('X', end=' ')
+            
+            if braille[i][4] == 1:
+                print('O', end=' ')
+            else:
+                print('X', end=' ')
+        print()
+        for i in range(braille_dict['number']):
+            if braille[i][2] == 1:
+                print('O', end=' ')
+            else:
+                print('X', end=' ')
+            
+            if braille[i][5] == 1:
+                print('O', end=' ')
+            else:
+                print('X', end=' ')
+        print()
         print('-'*10)
